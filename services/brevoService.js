9382abbe8006641de75ef5743dbff5006134bfc6
Aman Agrawal <582002aman@gmail.com>
2025-02-09 17:58:57 +0000

    Added thank you email template

--- services/brevoService.js
+++ services/brevoService.js
@@ -1,4 +1,6 @@
 const SibApiV3Sdk = require('sib-api-v3-sdk');
+const path = require('path');
+const fs = require('fs');
 
 const apiKey = process.env.BREVO_API_KEY;
 const client = SibApiV3Sdk.ApiClient.instance;
@@ -37,11 +39,21 @@ exports.sendThankYouEmail = async (firstName, email) => {
   const senderEmail = process.env.BREVO_SENDER_EMAIL;
   const senderName = process.env.BREVO_SENDER_NAME;
 
+  const templatePath = path.join(
+    __dirname,
+    '../templates/thank_you_email.html'
+  );
+  let emailHtml = fs.readFileSync(templatePath, 'utf-8');
+  emailHtml = emailHtml.replace('{{firstName}}', firstName);
+
+  const emailText = `Hi ${firstName},\n\nThanks for signing up! 🎉 We're excited to have you on board.\n\nStay tuned for valuable insights and exclusive content!\n\nBest,\n${senderName}\n(This is an automated email. Please do not reply.)`;
+
   const emailData = {
     sender: { email: senderEmail, name: senderName },
     to: [{ email, name: firstName }],
     subject: 'Thank You for Signing Up!',
-    htmlContent: `<p>Hi ${firstName},</p><p>Thank you for signing up!</p>`,
+    htmlContent: emailHtml,
+    textContent: emailText,
   };
 
   await emailApi.sendTransacEmail(emailData);
