ac7da2024ddf17db1d9787def445f60263d7802c
Aman Agrawal <582002aman@gmail.com>
2025-02-09 13:05:41 +0000

    Made it api endpoint for code reusability

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -3,13 +3,29 @@ const { addToBrevo, sendThankYouEmail } = require('../services/brevoService');
 
 exports.handleFormSubmission = async (req, res) => {
   const { firstName, lastName, email } = req.body;
+  const isAPI = req.query.api === 'true';
 
   try {
     await addToBrevo(firstName, lastName, email);
     await sendThankYouEmail(firstName, email);
-    res.sendFile(path.join(__dirname, '../views/success.html'));
+    if (isAPI) {
+      return res.json({
+        success: true,
+        message: 'Thank you for signing up!',
+      });
+    } 
+    return res.sendFile(path.join(__dirname, '../views/success.html'));
   } catch (error) {
-    console.error(error);
-    res.sendFile(path.join(__dirname, '../views/failure.html'));
+    console.error(
+      'Error adding contact:',
+      error.response?.body || error.message
+    );
+    if (isAPI) {
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+    return res.sendFile(path.join(__dirname, '../views/failure.html'));
   }
 };
